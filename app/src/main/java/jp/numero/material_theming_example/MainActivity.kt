7160cac83a3da254c6324695d79d2f2b35b8cad0
NUmeroAndDev <numero.dev.and@gmail.com>
2019-02-11 17:31:53 +0900

    Restore menu

--- app/src/main/java/jp/numero/material_theming_example/MainActivity.kt
+++ app/src/main/java/jp/numero/material_theming_example/MainActivity.kt
@@ -1,6 +1,8 @@
 package jp.numero.material_theming_example
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,6 +17,19 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_info -> true
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
